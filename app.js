6a31e7bd33d75b531fe05f9864f6a35218c678f0
William Liandri <30991741+wliandri@users.noreply.github.com>
2018-03-26 20:00:21 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+// Import the express
 const express = require('express');
 const router = require('./routes/userRoute');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Set the app to be able to use ejs.
 app.set('view engine','ejs');
 
 app.use('/',router);
 
+// Start the app at the Port point
 app.listen(PORT,function(){
     console.log(`Express listening on port ${PORT}`);
 });
