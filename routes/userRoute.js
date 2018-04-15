771161000e70d123eb8b36492447c4c8bb15ddcc
William Liandri <30991741+wliandri@users.noreply.github.com>
2018-04-15 22:12:30 +1000

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,4 +8,10 @@ router.get('/users', control.printAll);
 
 router.get('/users/:id', control.printUser);
 
+router.get('/diseases', control.diseaseWiki);
+
+router.get('/disease/:id', control.disease);
+
+router.get('/profile', control.profile);
+
 module.exports = router;
