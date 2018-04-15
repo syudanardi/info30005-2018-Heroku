4f102a349d06a4421fd724797a2ff1a040aac401
William Liandri <30991741+wliandri@users.noreply.github.com>
2018-04-15 22:10:31 +1000

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,3 +18,27 @@ module.exports.printUser = function(req, res) {
         job:db.dataBase[req.params.id].job
     })
 };
+
+module.exports.diseaseWiki = function(req, res) {
+    res.locals.query = req.query;
+    res.render("diseasewiki", {alphabet: db.alphabet, diseases: db.diseases
+    })
+};
+
+
+module.exports.disease = function(req, res) {
+    res.locals.query = req.query;
+    var alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+    // Get the index of the alphabet in the array. 
+    var index = alphabets.indexOf(req.params.id);
+    res.render("disease", {alphabet: db.alphabet, 
+        chooseAlphabet: db.alphabet[index],
+        disease: db.diseases[index],
+        diseases: db.diseases
+    })
+}
+
+module.exports.profile = function(req, res) {
+    res.render("profile", {profile: db.profile, diseases: db.diseases})
+}
