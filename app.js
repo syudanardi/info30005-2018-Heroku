a07df6c665d9075deead4271cb24f3db6200c0bb
William Liandri <30991741+wliandri@users.noreply.github.com>
2018-04-15 22:13:26 +1000

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.set('view engine','ejs');
 
 app.use('/',router);
 
+app.use(express.static('public'));
+
+app.use(express.static('resources'));
+
 app.listen(PORT,function(){
     console.log(`Express listening on port ${PORT}`);
 });
