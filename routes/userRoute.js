4a0bf384ff89295f3f63beac3ec7be43a4640d94
William Liandri <wliandri@student.unimelb.edu.au>
2018-04-16 15:54:31 +1000

    Add route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -10,11 +10,6 @@ router.get('/users', control.printAll);
 
 router.get('/users/:id', control.printUser);
 
-<<<<<<< HEAD
-
-
-module.exports = router;
-=======
 router.get('/diseases', control.diseaseWiki);
 
 router.get('/disease/:id', control.disease);
@@ -22,4 +17,3 @@ router.get('/disease/:id', control.disease);
 router.get('/profile', control.profile);
 
 module.exports = router;
->>>>>>> 06c0186c59e1344bc3af3597de793a6d1473f93c
