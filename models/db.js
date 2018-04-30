b1ec56b02d9856c92c526244ebf1d6a588dd7d94
William Liandri <w.liandri@student.unimelb.edu.au>
2018-04-30 13:46:22 +1000

    Fix profile page

--- models/db.js
+++ models/db.js
@@ -12,8 +12,6 @@ mongoose.connect('mongodb://quickhealthdb:12345678@ds161539.mlab.com:61539/quick
 require('./disease.js');
 require('./user.js');
 
-const faker = require('faker');
-
 var dataBase = [];
 
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
@@ -22,26 +20,6 @@ var diseases = ["Asthma", "Bronchitis", "Calculi", "Deafness", "Encopresis", "Fi
 "Malaria", "Nephrosclerosis", "Obesophobia", "Pachygyria", "Quinsy", "Rabies",
 "Scleroderma", "Taeniasis", "Uremia", "Vipoma", "Warkany Syndrome", "Xanthinuria",
 "Yellow Fever", "Zygomycosis"];
-
-var profile = {
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        joinedMonth: "April",
-        joinedYear: 2017,
-        occupation: "Student",
-        from: "Sydney",
-        livesIn: "Melbourne",
-        mobile: 61424567890,
-        address: "800 Swanston Street, Melbourne VIC 3000, Australia",
-        email: "iamastudent@yahoo.com",
-        allergies: ["Doxycycline", "Stemetil prochlorperazine", "Amoxycillin"]
-    };
-
-
-var x;
-for (x=0;x<10;x++){
-    dataBase.push({name:faker.name.firstName(), job:faker.name.jobTitle()});
-}
 
 module.exports = dataBase;
 
