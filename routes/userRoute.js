e68da5418bbc33794e21f5c34ed541a1babd85b6
Unknown <gcipta@student.unimelb.edu.au>
2018-04-30 13:40:46 +1000

    Updated Home Page

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const control = require('../controllers/userController');
 
+router.get('/homepage', control.home)
+
 router.get('/home', control.realHome);
 
 router.get('/users', control.printAll);
