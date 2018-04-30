bf0af6ac3a6127819de6f6fe613d6167393ef3aa
William Liandri <w.liandri@student.unimelb.edu.au>
2018-04-30 14:00:29 +1000

    Remove static data on db.js and fix diseasewiki, disease, and profile page

--- models/db.js
+++ models/db.js
@@ -11,22 +11,3 @@ mongoose.connect('mongodb://quickhealthdb:12345678@ds161539.mlab.com:61539/quick
 
 require('./disease.js');
 require('./user.js');
-
-var dataBase = [];
-
-var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var diseases = ["Asthma", "Bronchitis", "Calculi", "Deafness", "Encopresis", "Fibromatosis", "Geliphobia", 
-"Headache", "Influenza", "Jones Syndrome", "Katz Syndrome", "Labyrinthitis",
-"Malaria", "Nephrosclerosis", "Obesophobia", "Pachygyria", "Quinsy", "Rabies",
-"Scleroderma", "Taeniasis", "Uremia", "Vipoma", "Warkany Syndrome", "Xanthinuria",
-"Yellow Fever", "Zygomycosis"];
-
-module.exports = dataBase;
-
-module.exports = {
-    dataBase: dataBase,
-    length: x,
-    alphabet: alphabet,
-    diseases: diseases,
-    profile: profile
-};
