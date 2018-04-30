1b8dd5fb09d54e99a65aeaea506af819d3b52e26
William Liandri <w.liandri@student.unimelb.edu.au>
2018-04-30 13:24:53 +1000

    Merge with Master

--- models/healthfacts.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var diseaseSchema = mongoose.Schema(
-    {
-        fact: String,
-        link: String
-    }
-);
-module.exports = mongoose.model('healthfacts', diseaseSchema);
