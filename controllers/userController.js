98e746f70c588103c8dec2d3308bb113782210eb
Gavin Wijaya <gcipta@student.unimelb.edu.au>
2018-05-09 10:25:07 +1000

    Cleaning up UI

--- controllers/userController.js
+++ controllers/userController.js
@@ -209,7 +209,8 @@ module.exports.createProfile = function(req, res) {
     });
     newProfile.save(function(err, newProfile){
         if(!err) {
-            res.send(newProfile);
+            //res.send(newProfile);
+            res.render("homepage_revised");
             console.log("New Profile Created\n");
         } else {
             res.sendStatus(400);
