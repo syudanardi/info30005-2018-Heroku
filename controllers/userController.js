6d15aca9c252a21de43fa5ba6c4686137e3a24ba
William Liandri <w.liandri@student.unimelb.edu.au>
2018-05-15 11:07:23 +1000

    Try save country value

--- controllers/userController.js
+++ controllers/userController.js
@@ -369,10 +369,36 @@ module.exports.createProfile = function(req, res) {
                 if(!err) {
                     QQ.find(function(err,quickquiz) {
                         if(!err) {
-                            res.render("homepage_revised", {
-                                qfdb:quickfacts,
-                                qqdb:quickquiz
+                            FeaturedVideos.find(function(err, video){
+                                
+                                if (!err) {
+                                    LocationNews.find(function(err, locnews) {
+                                        if (!err) {
+                                            TrendNews.find(function(err, trendnews) {
+                                                if (!err) {
+                                                    OutbreakNews.find(function(err, outbreaknews) {
+                                                        res.render("homepage_revised", {
+                                                            qfdb:quickfacts,
+                                                            qqdb:quickquiz,
+                                                            vid: video,
+                                                            locnews: locnews,
+                                                            trendnews: trendnews,
+                                                            outbreaknews: outbreaknews
+                                                        });
+                                                    });
+                                                } else {
+                                                    res.sendStatus(400);
+                                                }
+                                            });
+                                        } else {
+                                            res.sendStatus(400);
+                                        }
+                                    });
+                                } else {
+                                    res.sendStatus(400);
+                                }
                             });
+        
                         } else {
                             res.sendStatus(400);
                         }
